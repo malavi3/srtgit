d39d3dd67ed54c514d26ea7064a131b5b3a70f82
Khashayar <malavi3@myseneca.ca>
2019-02-17 17:14:34 -0800

    Todo 10

--- assignment0.R
+++ assignment0.R
@@ -1,7 +1,3 @@
-x1=rnorm(100)
-x2=rnorm(100)
-x3=rnorm(100)
-t=data.frame(a=x1, b=(x1+x2), c=(x1+x2+x3))
-plot(t$a, type="l", ylim=range(t), lwd=3, col=rgb(1,0,0,0.3))
-lines(t$b, type="s", lwd=2, col=rgb(0.3,0.4,0.3,0.9))
-points(t$c, pch=20, cex=4, col=rgb(0,0,1,0.3))
+d2 = read.table(file="~/srtgit/tst1.txt", header=TRUE)
+d2$g <- d2$g*5
+write.table(d2, file="~/srtgit/tst2.txt", row.names=FALSE)
